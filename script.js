ddc043cea601f47e2159fa27adc5078f07cb846d
AliKakoo <alikakuo@yahoo.com>
2025-01-30 15:07:13 +0330

    add final codes

--- script.js
+++ script.js
@@ -6,26 +6,45 @@ const generateBtn = $.querySelector("#submit");
 const codeTextarea = $.querySelector("#code");
 const copyBtn = $.querySelector("#copy");
 
-const setDirection = (direction , directionElem) => {
-    for (let  item  of directions) {
-        item.classList.remove('active')
-    }
+let currentDirection = "to bottom";
 
-    if (directionElem.tagName === 'BUTTON') {
-        directionElem.classList.add('active');
-        
-    } else {
-        directionElem.parentElement.classList.add('active');
-    }
-    
-  console.log(direction , directionElem);
+const setDirection = (direction, directionElem) => {
+  for (let item of directions) {
+    item.classList.remove("active");
+  }
+
+  if (directionElem.tagName === "BUTTON") {
+    directionElem.classList.add("active");
+  } else {
+    directionElem.parentElement.classList.add("active");
+  }
+
+  currentDirection = direction;
+};
+
+const genrateCssCode = () => {
+  let cssCode = `background :linear-gradient(${currentDirection},${firstColorInput.value},${secondColorInput.value})`;
+
+  codeTextarea.value = cssCode;
+
+  document.body.style.cssText = cssCode;
+};
+
+const copyCssCode = () => {
+  codeTextarea.select();
+  document.execCommand("copy");
+
+  alert('Css Code Copied 😊')
 };
 
 directions.forEach((direction) => {
   direction.addEventListener("click", (event) => {
-
     let direction = event.target.dataset.direction;
-    setDirection(direction , event.target);
-
+    setDirection(direction, event.target);
   });
 });
+
+generateBtn.addEventListener('click', genrateCssCode)
+copyBtn.addEventListener('click', copyCssCode)
+
+genrateCssCode();
